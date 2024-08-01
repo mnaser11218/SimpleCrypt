62e4e31c787d42e3e474e42a69e1e3a3f387047b
Mohammed <mohammed@Zipcoders-MacBook-Pro-7.local>
2024-08-01 17:17:28 -0400

    completed

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -3,8 +3,12 @@ import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
+    Character cs;
+    Character cf;
 
     ROT13(Character cs, Character cf) {
+        this.cs = cs;
+        this.cf = cf;
     }
 
     ROT13() {
@@ -13,20 +17,46 @@ public class ROT13  {
 
     public String crypt(String text) throws UnsupportedOperationException {
 
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if       (c >= 'a' && c <= 'm') {c += 13;}
+            else if  (c >= 'A' && c <= 'M') {c += 13;}
+            else if  (c >= 'n' && c <= 'z') {c -= 13;}
+            else if  (c >= 'N' && c <= 'Z') {c -= 13;}
+            System.out.println(c);
+            sb.append(c);
+        }
+        return sb.toString();
+       // return "testing";
+
     }
 
     public String encrypt(String text) {
-        return text;
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < text.length(); i++) {
+//            char c = text.charAt(i);
+//            if       (c >= 'a' && c <= 'm') {c += 13;}
+//            else if  (c >= 'A' && c <= 'M') {c += 13;}
+//            else if  (c >= 'n' && c <= 'z') {c -= 13;}
+//            else if  (c >= 'N' && c <= 'Z') {c -= 13;}
+//            System.out.println(c);
+//            sb.append(c);
+//        }
+//        return sb.toString();
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
 
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        String firstHalf = s.substring(s.indexOf(c));
+        String secondHalf = s.substring(0,s.indexOf(c) );
+        return stringBuilder.append(firstHalf).append(secondHalf).toString();
     }
 
 }
